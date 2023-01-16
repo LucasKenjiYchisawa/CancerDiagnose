3a967627ba8668dfdc6d70a512139d2ea9eb44ba
LucasKenjiYchisawa <122761834+LucasKenjiYchisawa@users.noreply.github.com>
2023-01-16 11:22:01 +0900

    Add files via upload

---
+++ SVM.py
@@ -0,0 +1,29 @@
+import sklearn
+from sklearn import datasets
+from sklearn import svm
+from sklearn.externals import joblib
+
+#Loading data from the dataset
+cancer = datasets.load_breast_cancer()
+
+#Splitting the data into input and output
+x = cancer.data
+y = cancer.target
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.2)
+#Using SVM with a linear kernel and 3 points in the soft margin( aka points that are not counted in the creation of hyperplane)
+clf=svm.SVC(kernel=typeOfKernel, degree= polynomialDegree, c=numberofPointsInTheSoftMargin)
+
+#training the model
+clf.fit(x_train,y_train)
+
+#using the model
+y_pred=clf.predict(x_test)
+
+#checking the accuracy
+acc=metrics.accuracy_score(y_test,y_pred)
+
+#Saving the trained model in an external file
+
+joblib.dump(clf, 'trained_model.joblib')
+
